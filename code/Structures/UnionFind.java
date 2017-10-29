e91e6fb77d4d87f739a4ebc8a8be76d6963d3927
Sebastian-0 <melon.bag@hotmail.com>
2017-10-29 16:37:58 +0100

    Added Union-Find.

---
+++ code/Structures/UnionFind.java
@@ -0,0 +1,25 @@
+public class UnionFind {
+  public Node find(Node n) {
+    if (n.parent != n)
+      n.parent = find(n.parent);
+    return n.parent;
+  }
+  
+  public void union(Node a, Node b) {
+    Node ra = find(a);
+    Node rb = find(b);
+    if (ra.rank > rb.rank) {
+      rb.parent = ra;
+    } else if (ra.rank > rb.rank) {
+      ra.parent = rb;
+    } else {
+      ra.parent = rb;
+      rb.rank++;
+    }
+  }
+  
+  static class Node {
+    Node parent = this;
+    int rank = 1;
+  }
+}

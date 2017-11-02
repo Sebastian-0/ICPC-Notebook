c521f779ce0090b5ebd4fb1574181fe692768ca4
Mattias Gustafsson <dat13mgu@student.lu.se>
2017-11-02 14:22:43 +0100

    Updated segment trees with an update method

--- code/Structures/SegmentTreeRMQ.java
+++ code/Structures/SegmentTreeRMQ.java
@@ -1,4 +1,4 @@
-private class SegmentTreeRMQ {
+public class SegmentTreeRMQ {
   public int[] segmentTree;
   public int length;
   
@@ -14,12 +14,13 @@ private class SegmentTreeRMQ {
   private int construct(int[] input, int low, 
       int hi, int i) {
     if (low >= input.length)
-      return Integer.MAX_VALUE;
+      return Integer.MAX_VALUE; //or min / 0
     if (low == hi) {
-      segmentTree[i] = input[low]; //can replace
+      segmentTree[i] = input[low];
       return input[low];
     }
     int mid = (low + hi) / 2;
+    //can replace with max / sum
     segmentTree[i] = Math.min(
         construct(input, low, mid, 2*i + 1),
         construct(input, mid+1, hi, 2*i + 2));
@@ -35,10 +36,22 @@ private class SegmentTreeRMQ {
     if (queryLow <= segLow && queryHi >= segHi)
       return segmentTree[i];
     if (queryLow > segHi || queryHi < segLow)
-      return Integer.MAX_VALUE;
+      return Integer.MAX_VALUE; //or min / 0
     int mid = (segLow + segHi) / 2;
-    return Math.min(
+    return Math.min( //or max / sum
       find(segLow, mid, queryLow, queryHi, 2*i + 1),
       find(mid+1, segHi, queryLow, queryHi, 2*i + 2));
+  }
+  
+  public void update(int index, int value) {
+    index = segmentTree.length/2 + index;
+    segmentTree[index] = value;
+    while (index > 0) {
+      index = (index - 1) / 2;
+      //or max / sum
+        segmentTree[index] = Math.min(
+            segmentTree[index*2+1],
+            segmentTree[index*2+2]);
+    }
   }
 }
